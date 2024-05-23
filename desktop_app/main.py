230d47ac0c0689abcae21acf433ba7e8bd3617f6
Cristian Nita <cristiannita24@gmail.com>
2024-05-23 11:09:42 +0300

    Add background subtraction

--- desktop_app/main.py
+++ desktop_app/main.py
@@ -3,22 +3,22 @@ import sys
 import cv2
 from PyQt5.QtCore import QTimer
 from PyQt5.QtGui import QImage, QPixmap
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout
 
 
 class CameraApp(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.start_button = None
-        self.camera_label = None
-        self.layout = None
-        self.stop_button = None
         self.init_ui()
 
         self.capture = cv2.VideoCapture(0)
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update_frame)
+
+        # Background subtractor
+        self.bg_subtractor = cv2.createBackgroundSubtractorMOG2()
+        self.show_subtraction = False
 
     def init_ui(self):
         self.setWindowTitle("Camera Surveillance System")
@@ -29,15 +29,22 @@ class CameraApp(QWidget):
         self.camera_label = QLabel(self)
         self.layout.addWidget(self.camera_label)
 
+        button_layout = QHBoxLayout()
+
         self.start_button = QPushButton("Start Camera", self)
         self.start_button.clicked.connect(self.start_camera)
-        self.layout.addWidget(self.start_button)
+        button_layout.addWidget(self.start_button)
 
         self.stop_button = QPushButton("Stop Camera", self)
         self.stop_button.clicked.connect(self.stop_camera)
         self.stop_button.setEnabled(False)
-        self.layout.addWidget(self.stop_button)
+        button_layout.addWidget(self.stop_button)
 
+        self.toggle_button = QPushButton("Enable Background Subtraction", self)
+        self.toggle_button.clicked.connect(self.toggle_subtraction)
+        button_layout.addWidget(self.toggle_button)
+
+        self.layout.addLayout(button_layout)
         self.setLayout(self.layout)
 
     def start_camera(self):
@@ -52,10 +59,22 @@ class CameraApp(QWidget):
         self.stop_button.setEnabled(False)
         self.camera_label.clear()
 
+    def toggle_subtraction(self):
+        self.show_subtraction = not self.show_subtraction
+        if self.show_subtraction:
+            self.toggle_button.setText("Disable Background Subtraction")
+        else:
+            self.toggle_button.setText("Enable Background Subtraction")
+
     def update_frame(self):
         ret, frame = self.capture.read()
         if ret:
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            if self.show_subtraction:
+                fg_mask = self.bg_subtractor.apply(frame)
+                frame = cv2.cvtColor(fg_mask, cv2.COLOR_GRAY2RGB)
+            else:
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
             h, w, ch = frame.shape
             bytes_per_line = ch * w
             qt_image = QImage(frame.data, w, h, bytes_per_line, QImage.Format_RGB888)
